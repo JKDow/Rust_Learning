d333f789afa20aa9badbb03fe07dff293af7250c
JKDow <114895228+JKDow@users.noreply.github.com>
2023-01-15 14:14:49 +1100

    Refined hash map in advent 2

--- advent_2_rpc_2/src/main.rs
+++ advent_2_rpc_2/src/main.rs
@@ -10,35 +10,30 @@ Last updated: 13/1/2023
 use std::io::{BufRead, BufReader};
 use std::fs::File;
 use std::collections::HashMap;
+
 struct Move {
     points: u32,
     win: String,
     lose: String,
 }
 
+impl Move {
+    fn new(points: u32, win: &str, lose: &str) -> Move {
+        Move { 
+            points,
+            win: String::from(win),
+            lose: String::from(lose),
+        }
+    }
+}
+
 fn main() {
     println!("Starting Main");
 
-    let rock = Move {
-        points: 1,
-        win: String::from("C"),
-        lose: String::from("B"),
-    };
-
-    let paper = Move {
-        points: 2,
-        win: String::from("A"),
-        lose: String::from("C"),
-    };
-
-    let scissors = Move {
-        points: 3,
-        win: String::from("B"),
-        lose: String::from("A"),
-    };
-    
     let mut str_to_move = HashMap::new();
-    str_to_move.insert("A".to_string(), rock); 
-    str_to_move.insert("B".to_string(), paper);
-    str_to_move.insert("C".to_string(), scissors); 
+    str_to_move.insert("A".to_string(), Move::new(1, "C", "B")); // A = rock
+    str_to_move.insert("B".to_string(), Move::new(2, "A", "C")); // B = paper
+    str_to_move.insert("C".to_string(), Move::new(3, "B", "C")); // C = Scissors
+
+    
 }
