f5388cbb4b7256990dfc0cdf5a5aa623863d8bca
JKDow <114895228+JKDow@users.noreply.github.com>
2023-01-18 17:56:29 +1100

    Create advent 5 project

---
+++ advent_5_crates/src/lib.rs
@@ -0,0 +1,24 @@
+use std::io::BufReader;
+use std::fs::File;
+use std::time::Instant;
+
+fn read_file(path: String) -> BufReader<File> {
+    let data = match File::open(path) {
+        Ok(file) => file,
+        Err(_) => {
+            panic!("Could not open file"); 
+        }
+    }; 
+    BufReader::new(data)
+}
+
+struct BenchMarker {
+    start: Instant,
+    end: Duration,
+}
+
+fn start(){
+    println!("Startint Main");
+    let start = Instant::now();
+}
+//here
