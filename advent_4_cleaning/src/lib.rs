0793890ae6ddfd3c7d81b7cf6f5a2eba173cabff
JKDow <114895228+JKDow@users.noreply.github.com>
2023-01-18 15:37:15 +1100

    update common library

--- advent_4_cleaning/src/lib.rs
+++ advent_4_cleaning/src/lib.rs
@@ -1,25 +1,23 @@
-mod input {
-    use std::io::BufReader;
-    use std::fs::File;
-    struct InFile {
-        file_path: String,
-        reader: BufReader<File>,
-    }
+use std::io::BufReader;
+use std::fs::File;
+use std::time::Instant;
 
-    impl InFile {
-        fn new(file_path: String) -> Result<InFile, String> {
-            let my_input: InFile = InFile { 
-                file_path: file_path.clone(),
-                reader: BufReader::new(
-                    match File::open(file_path) {
-                        Ok(f) => f,
-                        Err(_) => {
-                            return Err(String::from("Error opening file"))
-                        }
-                    }
-                ),
-            };
-            Ok(my_input)
+fn read_file(path: String) -> BufReader<File> {
+    let data = match File::open(path) {
+        Ok(file) => file,
+        Err(_) => {
+            panic!("Could not open file"); 
         }
-    }
+    }; 
+    BufReader::new(data)
+}
+
+struct BenchMarker {
+    start: Instant,
+    end: Duration,
+}
+
+fn start(){
+    println!("Startint Main");
+    let start = Instant::now();
 }
