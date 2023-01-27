ef18d502a6505001200a37405f191f80086a4973
JKDow <114895228+JKDow@users.noreply.github.com>
2023-01-27 18:51:04 +1100

    cleaned code

--- advent_8/src/part_1.rs
+++ advent_8/src/part_1.rs
@@ -16,12 +16,12 @@ impl Forrest {
         let data_string = read_file_str(path);
         let data: Vec<Vec<u32>> = data_string
             .lines()
-            .map(|line| line
+            .map(|line| -> Vec<u32> {line
                 .chars()
                 .map(|c| c
                     .to_digit(10)
                     .unwrap())
-                .collect()
+                .collect()}
             )
             .collect();
         let size = data.len();
@@ -41,65 +41,31 @@ impl Forrest {
 
     fn is_viewable(&mut self, row: usize, column: usize, side: Side) -> bool {
         let mut max = self.max.remove(&side).unwrap();
-
+        let mut ret = false;
         let max_index = match side {
             Side::Left => row,
             Side::Right => row,
             Side::Top => column,
             Side::Bottom => column,
         };
-
         if self.heights[row][column] > max[max_index] {
             max[max_index] = self.heights[row][column];
             if self.viewable[row][column] == true {
-                self.max.insert(side, max);
-                return false; 
+                ret = false; 
+            } else {
+                self.viewable[row][column] = true;
+                ret = true;
             }
-            self.viewable[row][column] = true;
-            self.max.insert(side, max);
-            return true;
-        }
-        if row == 0 || column == 0 || row == self.data_size-1 || column == self.data_size-1 {
-            if self.viewable[row][column] == true {
-                self.max.insert(side, max);
-                return false; 
+        }else if self.viewable[row][column] == false {
+            if row == 0 || column == 0 || row == self.data_size-1 || column == self.data_size-1 {
+                self.viewable[row][column] = true;
+                ret = true;
             }
-            self.viewable[row][column] = true;
-            self.max.insert(side, max);
-            return true;
         }
         self.max.insert(side, max);
-        return false;
+        return ret;
     }
 
-    /*fn is_viewable(&mut self, row: usize, column: usize, side: Side) -> bool {
-        let &mut max = match side {
-            Side::Left => &mut self.left_max[row],
-            Side::Right => &mut self.right_max[row],
-            Side::Top => &mut self.top_max[column],
-            Side::Bottom => &mut self.bottom_max[column],
-        };
-        self.check_viewable(row, column, max)
-    }
-    */
-    fn check_viewable(&mut self, row: usize, column: usize, max: &mut u32) -> bool {
-        if self.heights[row][column] > *max {
-            *max = self.heights[row][column];
-            if self.viewable[row][column] == true {
-                return false; 
-            }
-            self.viewable[row][column] = true;
-            return true;
-        }
-        if row == 0 || column == 0{
-            if self.viewable[row][column] == true {
-                return false; 
-            }
-            self.viewable[row][column] = true;
-            return true;
-        }
-        return false;
-    }
 }
 
 pub fn run(input: &str) -> u32 {
@@ -107,32 +73,30 @@ pub fn run(input: &str) -> u32 {
     let mut viewable_counter = 0; 
     for i in 0..forrest.data_size {
         for j in 0..forrest.data_size {
-            //check row
-            if forrest.is_viewable(i, j, Side::Left) {
-                viewable_counter += 1;
+            if forrest.max.get(&Side::Left).unwrap()[i] < 9 {
+                if forrest.is_viewable(i, j, Side::Left) {
+                    viewable_counter += 1;
+                }
             }
-
-            if forrest.is_viewable(i, forrest.data_size-j-1, Side::Right) {
-                viewable_counter += 1;
+            if forrest.max.get(&Side::Right).unwrap()[i] < 9 {
+                if forrest.is_viewable(i, forrest.data_size-j-1, Side::Right) {
+                    viewable_counter += 1;
+                }
             }
-            //check column 
-            if forrest.is_viewable(j, i, Side::Top)  {
-                viewable_counter += 1;
+            if forrest.max.get(&Side::Bottom).unwrap()[j] < 9 {
+                if forrest.is_viewable(j, i, Side::Top)  {
+                    viewable_counter += 1;
+                }
             }
-            if forrest.is_viewable(forrest.data_size-j-1, i, Side::Bottom) {
-                viewable_counter += 1;
+            if forrest.max.get(&Side::Bottom).unwrap()[j] < 9 {
+                if forrest.is_viewable(forrest.data_size-j-1, i, Side::Bottom) {
+                    viewable_counter += 1;
+                }
             }
         }
     }
-
-    for row in forrest.viewable {
-        println!("{:?}", row);
-    }
-
     return viewable_counter;
 }
-
-
 
 #[cfg(test)] 
 mod tests {
