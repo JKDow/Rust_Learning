ed413726d7bd58fdf6cbb22df78780c4a9375972
JKDow <114895228+JKDow@users.noreply.github.com>
2023-02-02 15:45:32 +1100

    Created advent 9

---
+++ advent_9/src/main.rs
@@ -0,0 +1,15 @@
+/*
+Rope bridge challenge from: 
+https://adventofcode.com/2022/day/9
+
+Author: Josh Dowling
+Created: 2/2/2023
+Last updated: 2/2/2023 
+*/
+
+use advent_9::*;
+
+fn main() {
+    let _timer = program_timer::BenchMarker::new("Main");
+    println!("Part 1: {}", part_1::run("input_test.txt"));
+}
---
+++ advent_9/src/part_1.rs
@@ -0,0 +1,17 @@
+use crate::read_file_str;
+
+
+pub fn run(input: &str) -> u32 {
+    return 0;
+}
+
+#[cfg(test)] 
+mod tests {
+    use super::*;
+
+    #[test]
+    fn data_1() {
+        let result = run("input_test.txt");
+        assert_eq!(result, 13)
+    }
+}
